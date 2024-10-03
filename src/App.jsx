8cf48fb7d60b76dc4369ce7cb6fcdc63903843ef
Omid Ali Heidari <ali@Omid-Air.home>
2024-10-03 15:03:19 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import StarRating from "./Components"
+
+function App() {
+
+  return (
+    <>
+      <StarRating />
+    </>
+  )
+}
+
+export default App
